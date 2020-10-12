1c68017707ed20a8478883dc879b7abd280b5f47
= <=>
2020-10-12 07:21:54 -0700

    Updating dependancies, middleware system

--- dist/jobs/compress.d.ts
+++
@@ -1,3 +0,0 @@
-import { FormatData, Next } from "../formatter";
-declare const _default: (data: FormatData, next: Next) => Promise<void | Error | FormatData>;
-export default _default;
--- dist/jobs/open.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (data, next) => next(null, data);
-//# sourceMappingURL=open.js.map
