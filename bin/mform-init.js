02d40b585d19b9d3d0d68377fe5197dbb216e0ad
= <=>
2020-12-11 05:19:14 -0800

    restructured CLI.  Opens directories now

---
+++ bin/mform-init.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+
+program.option("-y --yes", "Quick install with base settings.");
--- bin/mform.js
+++ bin/mform.js
@@ -8,69 +8,17 @@ const {
   unlinkSync,
   mkdirSync,
 } = require("fs");
-const { join, resolve, dirname } = require("path");
+const { resolve, dirname } = require("path");
 const { formatter } = require("../dist/formatter");
 const lineDiff = require("line-diff");
 
 program
   .version("1.1.0")
-  .option("-i --input <file>", "The input file to format.")
-  .option("-o --output <file>", "The file to save the output too.")
-  .option(
-    "-d --diff",
-    "Only print the differences from the previous output file."
-  )
-  .option("-p --purge", "Purge any temp diff files.");
+  .description("A MUSHcode pre-processor.")
+  .command("run [path]", "run a Project or file.")
+  .alias("r")
+  .command("init [project]", "Initialize a new MUSHCode project.")
+  .alias("i")
+  .command("purge [file]", "Purge your system of code deltas from diff files.");
 
 program.parse(process.argv);
-
-// Get any diff file if it exists.
-const DIFF_PATH = resolve(
-  `${dirname(program.input)}/.tmp/${program.input.split("/").pop()}`
-);
-const exists = existsSync(DIFF_PATH);
-let diffIn = "";
-// if the program is diffing, check for a temp file in the cwd.
-if (program.diff) {
-  try {
-    if (!existsSync(resolve(`${dirname(program.input)}/.tmp`)))
-      mkdirSync(resolve(`${dirname(program.input)}/.tmp`));
-  } catch {
-    console.log("Unable to create .tmp file in current directory.");
-  }
-}
-
-try {
-  diffIn = exists && readFileSync(DIFF_PATH, { encoding: "utf-8" });
-  const file = readFileSync(program.input, { encoding: "utf-8" });
-
-  formatter.format(file, dirname(program.input)).then((data) => {
-    let output = [];
-    if (program.diff) {
-      const diff = new lineDiff(diffIn, data);
-      const tempOutput = data.split("\n");
-      writeFileSync(DIFF_PATH, data);
-      diff.changes.forEach((change) => {
-        if (change.changes) {
-          output.push(tempOutput[change.lineno - 1]);
-        }
-      });
-    }
-
-    if (program.output) {
-      writeFileSync(program.output, program.diff ? output : data);
-      console.log(`[MFORM]: Formatted: ${program.input} >> ${program.output}`);
-    } else {
-      console.log(program.diff ? output.join("\n") : data);
-    }
-
-    // Purge the diff file.
-    if (program.purge) {
-      try {
-        unlinkSync(DIFF_PATH);
-      } catch {}
-    }
-  });
-} catch (error) {
-  console.log(`[MFORM]: Unable to process file: ${error.message}`);
-}
