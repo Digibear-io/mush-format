1dfcb0da4d8c16921337af400363ea2311b2852a
Lem Canady <lem.canady@gmail.com>
2023-07-29 12:01:37 -0700

    removed console log from format file

--- src/formatter.ts
+++ src/formatter.ts
@@ -98,18 +98,3 @@ export class Formatter {
 export const formatter = new Formatter();
 
 export { Next };
-
-formatter
-  .format(
-    `
-// This line won't render
-&command.cmd #123 = $things:
-  @pemit %#=And Stuff. // this line will be added to the first.
-
-- // A single dash on a line makes an extra return in
-  // the compressed code
-
-@@ This comment will appear in the compressed code.
-@@ Great for leaving notes in your final compressed code block!`
-  )
-  .then((result) => console.log(result.data));
